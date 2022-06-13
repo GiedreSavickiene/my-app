07a87b79270713c659bc910bac04c6ef9e6e8cdf
GiedreSavickiene <78999134+GiedreSavickiene@users.noreply.github.com>
2022-06-13 21:19:37 +0300

    Modal

--- shop/src/Components/back/ProductEdit.jsx
+++ shop/src/Components/back/ProductEdit.jsx
@@ -1,20 +1,77 @@
+import { useContext, useRef, useState } from "react";
+import BackContext from "../../Contexts/BackContext";
+
+const empty = {
+    title: '',
+    price: '',
+    code: '',
+    description: ''
+};
+
+
+
+
 function ProductEdit(){
+    const { setCreateProductData } = useContext(BackContext);
+
+    const [inputs, setInputs] = useState(empty);
+
+    const handleInputs = (e, input) => setInputs(i => ({ ...i, [input]: e.target.value }));
+
+    const edit = () => {
+        setCreateProductData({...inputs, price: parseFloat(inputs.price)});
+        setInputs(empty);
+    }
+
+
     return(
         <div className="modal">
             <div className="modal-dialog modal-dialog-centered">
                 <div className="modal-content">
                     <div className="modal-header">
-                        <h5 className="modal-title">Modal title</h5>
+                        <h2>Redaguoti produktą</h2>
                         <button type="button" className="close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div className="modal-body">
-                        <p>Modal body text goes here.</p>
+                        <div className="card mt-4">
+                            <div className="card-body">
+                                <div className="container">
+                                    <div className="row">
+                                        <div className="col-12">
+                                            <div className="form-group">
+                                                <label className="fu gray">Pavadinimas:</label>
+                                                <input type="text" className="form-control" value={inputs.title} onChange={e => handleInputs(e, 'title')} />
+                                            </div>
+                                        </div>
+                                        <div className="col-4">
+                                            <div className="form-group">
+                                                <label className="fu gray">Kaina:</label>
+                                                <input type="text" className="form-control" value={inputs.price} onChange={e => handleInputs(e, 'price')} />
+                                            </div>
+                                        </div>
+                                        <div className="col-8">
+                                            <div className="form-group">
+                                                <label className="fu gray">Kodas:</label>
+                                                <input type="text" className="form-control" value={inputs.code} onChange={e => handleInputs(e, 'code')} />
+                                            </div>
+                                        </div>
+                                        <div className="col-12">
+                                            <div className="form-group">
+                                                <label className="fu gray">Aprašymas:</label>
+                                                <textarea className="form-control" rows="3" value={inputs.description} onChange={e => handleInputs(e, 'description')}></textarea>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                  
                     </div>
                     <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary">Close</button>
-                        <button type="button" className="btn btn-primary">Save changes</button>
+                        <button type="button" className="btn btn-outline-secondary fu up">Uzdaryti</button>
+                        <button type="button" className="btn btn-outline-primary" fu up>Issaugoti</button>
                     </div>
                 </div>
             </div>
