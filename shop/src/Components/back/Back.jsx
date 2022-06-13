185852026e749aaf83093c18e950e9a2279b3fee
GiedreSavickiene <78999134+GiedreSavickiene@users.noreply.github.com>
2022-06-13 20:34:34 +0300

    message

--- shop/src/Components/back/Back.jsx
+++ shop/src/Components/back/Back.jsx
@@ -11,18 +11,19 @@ import ProductCreate from './ProductCreate';
 function Back() {
 
     const [products, setProducts] = useState(null);
+    const [lastProductUpdate, setLastProductUpdate] = useState(Date.now());
 
     const [createProductData, setCreateProductData] = useState(null);
 
     useEffect(() => {
         axios.get('http://localhost:3003/admin/products')
         .then(res =>setProducts(res.data))
-    }, []);
+    }, [lastProductUpdate]);
 
     useEffect(() => {
-        
+        if(createProductData === null) return;
         axios.post('http://localhost:3003/admin/products', createProductData)
-        .then(res =>console.log(res.data))
+        .then(res =>setLastProductUpdate(Date.now()))
     }, [createProductData]);
 
   return (
