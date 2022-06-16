4ed5bceb350ea29877ff69aea8b995e6960a9eb1
GiedreSavickiene <78999134+GiedreSavickiene@users.noreply.github.com>
2022-06-16 19:08:21 +0300

    css image

--- shop/src/Components/front/Product.jsx
+++ shop/src/Components/front/Product.jsx
@@ -2,9 +2,27 @@ function Product({product}){
     return(
         <div className="product">
             <div className="product_bin">
-                {product.title}
+                <div className="product_bin_top">
+                    <h2>{product.title}</h2><i>{product.code}</i> 
+                 </div>
+                <div className="product_bin_mid">
+                    <div className="product_bin_mid_img">
+                        {
+                            product.photo ? <img src={product.photo} alt={product.title}/> : <img src='http://localhost:3000/no-image.jpg' alt={product.title} ></img>
+                        }
+                        
+                    </div>
+                     <div className="product_bin_mid_desc">
+                            <p>{product.description}</p>
+                    </div>
+                </div>
+                <div className="product_bin_bottom">
+                     <span>{product.price}</span>
+                    <button>Pirkti</button>
+                </div>
             </div>
         </div>
+        
     )
 }
 export default Product;
